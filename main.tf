00727dc615a21802d2b132a48fceb16adb76da49
Thomas Felix <tfelix@meshcloud.io>
2024-09-12 13:20:41 +0200

    wip

--- main.tf
+++ main.tf
@@ -1,16 +1,35 @@
-resource "null_resource" "trigger_github_action" {
-  # Use triggers to force re-run
-  triggers = {
-    always_run = timestamp()  # This changes every time you run `terraform apply`
+resource "terraform_data" "replacement" {
+
+  # This is required in order to use those variables in the destroy part.connection
+  # Sadly you can not directly reference var.<NAME> inside the destroy provisioner.
+  input = {
+    github_token = var.github_token,
+    github_repo = var.github_repo,
+    github_owner = var.github_owner,
+    workflow_branch = var.workflow_branch
+    workflow_destroy_file_name = var.workflow_destroy_file_name
+    building_block_run = var.building_block_run
   }
 
   provisioner "local-exec" {
+    when = create
     command = <<EOT
       curl -X POST \
       -H "Accept: application/vnd.github.v3+json" \
       -H "Authorization: token ${var.github_token}" \
-      https://api.github.com/repos/${var.github_owner}/${var.github_repo}/actions/workflows/${var.workflow_file_name}/dispatches \
-      -d '{"ref":"main", "inputs": {"key1":"value1","key2":"value2"}}'
+      https://api.github.com/repos/${var.github_owner}/${var.github_repo}/actions/workflows/${var.workflow_create_file_name}/dispatches \
+      -d '{"ref":"${var.workflow_branch}", "inputs": {"bb-run":"${var.building_block_run}"}}'
+    EOT
+  }
+
+  provisioner "local-exec" {
+    when = destroy
+    command = <<EOT
+      curl -X POST \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Authorization: token ${self.input.github_token}" \
+      https://api.github.com/repos/${self.input.github_owner}/${self.input.github_repo}/actions/workflows/${self.input.workflow_destroy_file_name}/dispatches \
+      -d '{"ref":"${self.input.workflow_branch}", "inputs": {"bb-run":"${self.input.building_block_run}"}}'
     EOT
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,22 +1,41 @@
 variable "github_token" {
   description = "GitHub Personal Access Token"
   type        = string
-  sensitive   = true
+  sensitive   = false
 }
 
 variable "github_owner" {
   default = "tfelix"
+  description = "The owner of the GitHub repo containing the workflow."
   type = string
 }
 
 variable "github_repo" {
   default = "tf-github-action-trigger"
+  description = "The name of the GitHub repo containing the workflow."
   type = string
 }
 
-variable "workflow_file_name" {
-  default = ".github/workflows/workflow.yml"
-  description = "The name of the workflow file (e.g. workflow.yml)"
+variable "workflow_create_file_name" {
+  default = "deploy.yml"
+  description = "The name of the workflow file (e.g. workflow.yml) for deploying the resources."
   type = string
+}
 
+variable "workflow_destroy_file_name" {
+  default = "destroy.yml"
+  description = "The name of the workflow file (e.g. workflow.yml) for destryoing the resources."
+  type = string
+}
+
+variable "workflow_branch" {
+  default = "main"
+  description = "The name of the branch in which the workflow files live."
+  type = string
+}
+
+variable "building_block_run" {
+  default = "{}"
+  description = "The content of the Building Block Run."
+  type = string
 }
